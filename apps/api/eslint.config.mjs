ca1ab6b67e0b576fbaf8eeb8dd2420bde260bd41
Pierre Poumayrac <pierre.poumayrac@agrotoken.io>
2025-04-08 20:37:41 -0300

    feat: turborepo configuration

---
+++ apps/api/eslint.config.mjs
@@ -0,0 +1,4 @@
+// @ts-check
+import config from '@monitor/eslint-config/nestjs.js';
+
+export default config;
--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  transpilePackages: ["@monitor/ui"],
+};
 
-export default nextConfig;
+module.exports = nextConfig;
